b388e3626a3eb139b42a852e97df7661fef62200
sobia <sobiashahbaz66@yahoo.com>
2020-03-08 22:52:11 +0200

    added minified images src in index.html

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,6 @@
 var menuBtn = document.querySelector('.main-menu__btn');
 var mainPage = document.querySelector('.main__page');
 
-
 var showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -9,10 +8,10 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     mainPage.classList.add("show-menu")
-    // jQuery(".main__page").addClass("show-menu");
     showMenu = true;
   } else {
     mainPage.classList.remove("show-menu")
+
     showMenu = false;
   }
 }
